5208ab000561d4d074432585c63bb1d21fac7226
Ashish poudel <techcode057@gmail.com>
2024-03-25 18:02:49 +0545

    private

---
+++ my-app/app/product/[productId]/review/[reviewId]/not-found.tsx
@@ -0,0 +1,9 @@
+
+export default function notFound() {
+    return(
+        <div className="">
+            <h1>page  not found</h1>
+        </div>
+    )
+    
+}
--- my-app/app/product/[productId]/review/[reviewId]/page.tsx
+++ my-app/app/product/[productId]/review/[reviewId]/page.tsx
@@ -1,3 +1,17 @@
-export default function  productDetails ({params } : {params : {reviewId : string ,  productId :  string} }) {
-    return <h1>review  of product {params.productId} by  {params.reviewId}   </h1>
+import { notFound } from "next/navigation";
+
+export default function productDetails({
+  params,
+}: {
+  params: { reviewId: string; productId: string };
+}) {
+    if (parseInt(params.reviewId ) > 1000) {
+        notFound();
+        
+    }
+  return (
+    <h1>
+      review of product {params.productId} by {params.reviewId}{" "}
+    </h1>
+  );
 }
